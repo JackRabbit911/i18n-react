77e12c0979b996fa83e4f1b2f397ccf49718f341
alx <world.zay@gmail.com>
2026-03-17 09:28:08 +0300

    #5 Add :lang dinamic segment to routes

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,17 +1,17 @@
-import ErrorCmp from "Layout/reused/ErrorCmp"
-import HomePage from "pages/HomePage.tsx"
 import Page1 from "pages/Page1"
 import Page2 from "pages/Page2"
 import Page3 from "pages/Page3"
+import HomePage from "pages/HomePage.tsx"
 import { Route, Routes } from "react-router"
+import ErrorCmp from "Layout/reused/ErrorCmp"
 
 const Router = () => (
   <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/page1' element={<Page1 />} />
-    <Route path='/page2' element={<Page2 />} />
-    <Route path='/page3' element={<Page3 />} />
-    <Route path='*' element={<ErrorCmp status={404} />} />
+    <Route path=':lang?/' element={<HomePage />} />
+    <Route path=':lang?/page1' element={<Page1 />} />
+    <Route path=':lang?/page2' element={<Page2 />} />
+    <Route path=':lang?/page3' element={<Page3 />} />
+    <Route path=':lang?/*' element={<ErrorCmp status={404} />} />
   </Routes>
 )
 
