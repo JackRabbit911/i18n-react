7555d783899d70f1dd37b778e2e3e0a90c71931d
alx <world.zay@gmail.com>
2026-03-18 14:48:17 +0300

    #11 Move BrowderRouter to up level and apply deps prop

--- src/i18n/TranslateProvider.tsx
+++ src/i18n/TranslateProvider.tsx
@@ -1,16 +1,19 @@
-import { useLocation } from "react-router";
 import { createContext, useEffect, useRef, useState } from "react";
 import { sprintf, updateTranslate } from "./utils";
 import { detectLang, getTranslate } from "./config";
-import type { Argv, ChildrenProps, TranslateContextType, TranslateType } from "./types";
+import type { Argv, TranslateContextType, TranslateType } from "./types";
 
 export const TranslateContext = createContext<TranslateContextType | undefined>(undefined)
 
-const TranslateProvider = ({ children }: ChildrenProps) => {
+type Props = {
+  deps?: React.DependencyList;
+  children?: React.ReactNode;
+}
+
+const TranslateProvider = ({ deps = [], children }: Props) => {
   const [translate, setTranslate] = useState<TranslateType>({})
   const [lang, setLang] = useState<string>(detectLang())
   const translateKeys = useRef<string[]>([])
-  const location = useLocation()
 
   const gettext = (key: string, ...argv: Argv) => {
     if (Object.hasOwn(translate, key)) {
@@ -34,7 +37,7 @@ const TranslateProvider = ({ children }: ChildrenProps) => {
           updateTranslate(translate, result, translateKeys, setTranslate)
         })
     }
-  }, [location.pathname])
+  }, deps)
 
   return (
     <TranslateContext.Provider value={{ gettext, setTranslate, lang, setLang }}>
