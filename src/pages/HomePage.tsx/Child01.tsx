4e4f7e40aa6f8babe5938cd832523cf0c736c872
alx <world.zay@gmail.com>
2026-03-19 17:09:44 +0300

    #18 Apply user type Type__

--- src/pages/HomePage.tsx/Child01.tsx
+++ src/pages/HomePage.tsx/Child01.tsx
@@ -1,6 +1,6 @@
-import type { GetTextProp } from "i18n/types"
+import type { Type__ } from "i18n/config"
 
-const Child01 = ({ __ }: GetTextProp) => {
+const Child01 = ({ __ }: Type__) => {
   return (
     <div className="ms-4 p-1 border border-zinc-600">
       <h2>{__('Page%', '01')}</h2>
--- src/pages/Page1/Child11.tsx
+++ src/pages/Page1/Child11.tsx
@@ -1,6 +1,6 @@
-import type { GetTextProp } from "i18n/types"
+import type { Type__ } from "i18n/config"
 
-const Child11 = ({ __ }: GetTextProp) => {
+const Child11 = ({ __ }: Type__) => {
   return (
     <div className="p-1 ms-4 border border-zinc-600">
       <h2>{__('Page%', '11')}</h2>
